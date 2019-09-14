0e972444dc55054b8c7276911864c956c69ad5fc
Dao Viet Anh <daovietanh99@Daos-MacBook-Pro.local>
2019-09-14 15:38:57 +0700

    event handle

--- components/MathLine.js
+++ components/MathLine.js
@@ -3,7 +3,16 @@ import MathJax from 'react-mathjax3'
  
 const MathLine = (props) => {
     return (
-        <div style={{fontSize: "1.5rem", padding: "0.5rem", opacity: props.opacity || 1, overflow: "hidden"}}>
+        <div style={{
+                fontSize: "1rem", 
+                padding: "0.5rem", 
+                opacity: props.opacity || 1, 
+                overflowX: "scroll", 
+                backgroundColor: props.color, 
+                borderRadius: "0.2rem",
+                cursor: "text"
+            }}
+        >
             <MathJax.Context input='ascii'>
                 <div>
                     <MathJax.Node>{props.ascii}</MathJax.Node>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Box} from 'gestalt';
+import ContentEditable from 'react-contenteditable';
 import MathLine from '../components/MathLine';
-import InputField from '../components/InputField';
 import "gestalt/dist/gestalt.css";
 import "./style.css";
 
@@ -9,41 +9,92 @@ class Home extends React.Component {
   constructor(props) {
     super(props)
     this.handleChange = this._handleChange.bind(this);
-    this.handleEnter = this._handleEnter.bind(this);
-    this.handleBackspace = this._handleBackspace.bind(this);
+    this.handleKeyDown = this._handleKeyDown.bind(this);
+    this.handleSubmit = this._handleSubmit.bind(this);
+    this.contentEditable = React.createRef();
     this.state = {
       lines: [],
-      value: ""
+      lineHTMLs: [],
+      value: "",
+      html: ""
     };
   }
 
-  _handleChange({ value }) {
+  _handleChange = evt => {
+    let obj = document.createElement("DIV");
+    obj.innerHTML = evt.target.value;
+    let value = obj.innerText;
+    let parts = value.split('"');
+    let html = obj.innerHTML;
+    //console.log(parts, obj.innerHTML)
+    // let html = "";
+    // for(let i = 0 ; i < parts.length; i++) {
+    //   html += (i%2 == 0 && parts[i] !== "") ? '<span class="string">' + parts[i] + '</span>': '<span class="fomular">' + parts[i] + '</span>';
+    //   if(i != parts.length-1) html += '"';
+    // }
     this.setState({
-      value
+      value: obj.innerText,
+      html: html
     });
+  };
+
+  _handleSubmit = evt => {
+    if(evt.key === 'Enter') {
+      this.setState({
+        lines: [...this.state.lines, this.state.value],
+        lineHTMLs: [...this.state.lineHTMLs, this.state.html],
+        value: "",
+        html: ""
+      })
+      evt.preventDefault();
+    }
   }
 
-  _handleEnter() {
-    let lines = this.state.lines;
-    lines.push(this.state.value);
-    this.setState({
-      lines: lines
-    })
-  }
-
-  _handleBackspace() {
-
+  _handleKeyDown = evt => {
+    switch (evt.key) {
+      case 'Backspace': 
+        if(this.state.value === "") {
+          evt.preventDefault();
+          let lines = this.state.lines;
+          let lineHTMLs = this.state.lineHTMLs;
+          this.setState({
+            value: (lines.pop() || "") ,
+            html: (lineHTMLs.pop() || "") ,
+            lines: lines || [],
+            lineHTMLs: lineHTMLs || []
+          })
+        }
+        break;
+      default: break;
+    }
   }
 
   render() {
     return (
       <Box display='flex'>
         <Box column={9}>
-          {this.state.lines.map((ele, index) => {
-            return <MathLine ascii={'"' + ele + '"'} key={index}/>
-          })}
-          <MathLine ascii={'"' + this.state.value + '"'} opacity={0.45}/>
-          <InputField onChange={this.handleChange} onEnter={this.handleEnter} onBackspace={this.handleBackspace}/>
+          <Box maxHeight="85vh" overflow="scrollY">
+            {this.state.lines.map((ele, index) => {
+              return <MathLine ascii={'"' + ele + '"'} key={index}/>
+            })}
+          </Box>
+          <Box maxHeight="15vh" height="15vh">
+            <MathLine ascii={'"' + this.state.value + '"'} opacity={0.45} color="lightgray"/>
+            <ContentEditable
+              innerRef={this.contentEditable}
+              html={this.state.html}
+              disabled={false}      
+              onChange={this.handleChange} 
+              onKeyDown={this.handleKeyDown}
+              onKeyPress={this.handleSubmit}
+              placeholder="Write Something Creative..."
+              tagName='div' 
+              className="input"
+            />
+          </Box>
+        </Box>
+        <Box column={3}>
+
         </Box>
       </Box>
     );
